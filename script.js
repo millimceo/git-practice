17456595c8e3596d416872c07772d07efa2c7766
MINKI KIM <millimceo@gmail.com>
2023-02-23 13:34:47 +0900

    what

---
+++ script.js
@@ -0,0 +1,15 @@
+/*textContent*/
+const myHeading = document.querySelector( 'h1' );
+myHeading.textContent = 'good';
+
+const myNav = document.querySelector( 'p' );
+myNav.textContent = 'navigation';
+
+const myDiv = document.querySelector('h2');
+myDiv.textContent = 'jesus';
+
+const theHeading = document.getElementById("flex");
+theHeading.textContent = 'what';
+
+const theAside = document.getElementById("realme");
+theAside.textContent = 'jesus2';
